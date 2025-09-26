38eb0eb5e97e9ea1cec5e811b2ff73b834ca786e
Mason <xmsica@163.com>
2025-09-26 15:54:31 -0600

    fix the item-list and item component

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,4 +1,5 @@
-export default function Item({ name, quantity, category }) {
+export default function Item({ item }) {
+  const { name, quantity, category } = item;
   return (
     <ul className="bg-slate-800 my-4 p-2 w-96">
       <li className="font-bold text-2xl">{name}</li>
